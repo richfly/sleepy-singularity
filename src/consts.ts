de4a41acbc828e0d98cbed40d83a98b81ad214b9
Rich Fly <rich@Richs-MacBook-Pro.local>
2023-12-10 11:18:55 -0800

    add tags

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Rich Fly";
-export const SITE_SUBHEADER = "Data, People, and Partnerships";
+export const SITE_SUBHEADER = "Mind of an Odd One";
 export const SITE_DESCRIPTION = "Hey there it's Rich!";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,25 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+
+  // Sort posts by date, newest first
+  const sortedPosts = posts.sort((a, b) => {
+    return new Date(b.data.pubDate) - new Date(a.data.pubDate);
+  });
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: sortedPosts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+      customData: post.data.tags
+        ? `<tags>${post.data.tags.join(", ")}</tags>`
+        : "",
+    })),
+  });
 }
