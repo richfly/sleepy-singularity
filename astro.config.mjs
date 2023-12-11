cd6d01ba5cb6c60f0d7416a4b5c8b53b02dcbefa
Rich Fly <rich@Richs-MacBook-Pro.local>
2023-12-11 15:34:56 -0800

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
