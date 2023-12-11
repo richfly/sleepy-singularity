8fd1e0315399b03f731c54759753443203b29716
Rich Fly <rich@Richs-MacBook-Pro.local>
2023-12-11 08:41:38 -0800

    site subheader removed

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Rich Fly";
-export const SITE_SUBHEADER = "Mind of an Odd One";
 export const SITE_DESCRIPTION = "Hey there it's Rich!";
